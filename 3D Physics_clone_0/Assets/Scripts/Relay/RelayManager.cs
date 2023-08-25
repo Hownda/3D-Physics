07cdf9d638e57ea0325542a8536db3f32622ae74
Hownda <117111277+Hownda@users.noreply.github.com>
2023-08-25 21:50:12 +0200

    Created Multiplayer Project

---
+++ 3D Physics_clone_0/Assets/Scripts/Relay/RelayManager.cs
@@ -0,0 +1,94 @@
+using ParrelSync;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Unity.Services.Relay.Models;
+using Unity.Services.Relay;
+using Unity.Netcode.Transports.UTP;
+using Unity.Netcode;
+using UnityEngine.UI;
+
+public class RelayManager : MonoBehaviour
+{
+    public GameObject networkPanel;
+    public InputField roomKeyInput;
+
+    private void Start()
+    {
+        Authentication();
+    }
+
+    public async void Authentication()
+    {
+        await Authenticate();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in as player id: " + AuthenticationService.Instance.PlayerId);
+        };
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    private async Task Authenticate()
+    {
+        var options = new InitializationOptions();
+
+#if UNITY_EDITOR
+        options.SetProfile(ClonesManager.IsClone() ? ClonesManager.GetArgument() : "Primary");
+#endif
+
+        await UnityServices.InitializeAsync(options);
+    }
+
+    public async void OnClickHost()
+    {
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(4);
+
+        try
+        {
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetHostRelayData(
+                allocation.RelayServer.IpV4,
+                (ushort)allocation.RelayServer.Port,
+                allocation.AllocationIdBytes,
+                allocation.Key,
+                allocation.ConnectionData
+                );
+
+            NetworkManager.Singleton.StartHost();
+            networkPanel.SetActive(false);
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    public async void OnClickClient()
+    {
+        try
+        {
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(roomKeyInput.text);
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetClientRelayData(
+                joinAllocation.RelayServer.IpV4,
+                (ushort)joinAllocation.RelayServer.Port,
+                joinAllocation.AllocationIdBytes,
+                joinAllocation.Key,
+                joinAllocation.ConnectionData,
+                joinAllocation.HostConnectionData
+                );
+
+            NetworkManager.Singleton.StartClient();
+            networkPanel.SetActive(false);
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
